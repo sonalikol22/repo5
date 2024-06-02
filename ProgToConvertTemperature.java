d549a6b70d6f04af164b7f9f1fc31ae9a7528e67
Sonalikol22 <sonalikol22@gmail.com>
2024-06-02 21:48:29 +0530

    repo 1 file commit

---
+++ ProgToConvertTemperature.java
@@ -0,0 +1,11 @@
+public class ProgToConvertTemperature {
+    public static void main(String[] args){
+        double c=12.3, f,k;
+       f=(1.8*c)+32;
+       System.out.println(c+" Celcius = " +f +" Fahrenheit");
+       k=c+273;
+        System.out.println(c+" Celcius = " +k +" Kelvin");
+
+
+    }
+}
