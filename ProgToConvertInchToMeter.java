102c14d1e2c92b63fe2268c112a8e64fb9d46021
Sonalikol22 <sonalikol22@gmail.com>
2024-06-02 23:24:40 +0530

    repo 1 file commit

---
+++ ProgToConvertInchToMeter.java
@@ -0,0 +1,10 @@
+public class ProgToConvertInchToMeter {
+    public static void main(String[] args) {
+        double inch = 10.0, meter;
+
+        meter = inch * 0.0254;
+        System.out.println(meter);
+    }
+}
+
+
