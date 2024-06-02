bdb1b25d18f3df45df08067a7b8e654e17d4c5a8
Sonalikol22 <sonalikol22@gmail.com>
2024-06-02 23:38:53 +0530

    repo 1 file commit

---
+++ ProgToFinsSquareCube.java
@@ -0,0 +1,16 @@
+public class ProgToFinsSquareCube {
+    public static void main(String[] args){
+        //Tried for -45, 2, 34.5 and 89
+        int i=-45;
+        System.out.println(("Square of a number:-"+ (i*i)));
+        System.out.println(("cube of a number:-"+ (i*i*i)));
+        System.out.println(("4th power of a number:-"+ (i*i*i*i)));
+        System.out.println("---------------------------------------------------");
+        Double d= 34.5;
+        System.out.println(("Square of a number:-"+ (d*d)));
+        System.out.println(("cube of a number:-"+ (d*d*d)));
+        System.out.println(("4th power of a number:-"+ (d*d*d*d)));
+
+
+    }
+}
