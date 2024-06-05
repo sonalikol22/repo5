126ce9f073e9254c2a1f8f538c6b113d40ceb7c6
Sonalikol22 <sonalikol22@gmail.com>
2024-06-05 22:45:08 +0530

    repo 1 file commit

---
+++ NoOfDaysInMonth.java
@@ -0,0 +1,10 @@
+public class NoOfDaysInMonth {
+    public static void main(String[] args){
+        int month=1;
+        if(month==1)
+            System.out.println("Number of days is 31");
+        else
+            System.out.println("Number of days is 30");
+
+    }
+}
