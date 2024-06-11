fac079c19067c743d5449ecbe04e06c5ab4ab158
Sonalikol22 <sonalikol22@gmail.com>
2024-06-11 11:40:49 +0530

    repo 1 file commit

---
+++ InfiniteForLoopDemo.java
@@ -0,0 +1,11 @@
+public class InfiniteForLoopDemo
+{
+    public static void main(String[] args){
+        // first way for Infinite for loop
+        for(; ;){
+            System.out.println("Hello");
+        }
+
+        //Second way for Infinite loop
+    }
+}
