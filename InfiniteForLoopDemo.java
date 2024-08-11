1fb084d69a28314cbb9beeb278e827669a9701ea
Sonalikol22 <sonalikol22@gmail.com>
2024-08-11 21:33:45 +0530

    print table file commit

--- InfiniteForLoopDemo.java
+++ InfiniteForLoopDemo.java
@@ -2,10 +2,41 @@ public class InfiniteForLoopDemo
 {
     public static void main(String[] args){
         // first way for Infinite for loop
-        for(; ;){
-            System.out.println("Hello");
-        }
+//        for(; ;){
+//            System.out.println("Hello");
+//        }
 
         //Second way for Infinite loop
+        for(int i=0;i<3;i--){
+            System.out.println("hello");
+        }
+
+
+
+//        int x=10;
+//        int z=20;
+//        int y=x+++z;
+//        System.out.print(y);
+//        int i=12/4*8/2;
+//        System.out.println(i);
+//        short s1=(byte)130;
+//        System.out.println(s1);
+//        double d1=(float)(double)(long)34.2d;
+//        System.out.println(d1);
+//        int i=2^2^3;
+//        System.out.println(i);
+      //  System.out.println(3*0.1==0.3);
+//        short s1=(byte)120;
+//        System.out.println(s1);
+//        int i1=20;
+//        byte b1=(byte)(short)i1;
+//        System.out.println(b1);
+//        double d1=(long)(double)34;
+//        System.out.println(d1);
+        int i=10;
+        System.out.println(i<<1);
+
+
     }
+
 }
